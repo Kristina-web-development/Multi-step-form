b7ba2cc55fb95d1c507def534fe7aed91b847871
Kristina-web-development <99411070+Kristina-web-development@users.noreply.github.com>
2024-08-12 20:23:25 +0300

    start

---
+++ src/style/View/Layout.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Layout = () => {
+  return (
+    <nav>
+        <div>
+            <p>1</p>
+            <p>first</p>
+            <p>your text</p>
+        </div>
+        <div>
+            <p>2</p>
+            <p>first</p>
+            <p>your text</p>
+        </div>
+        <div>
+            <p>3</p>
+            <p>first</p>
+            <p>your text</p>
+        </div>
+        <div>
+            <p>4</p>
+            <p>first</p>
+            <p>your text</p>
+        </div>
+    </nav>
+  )
+}
+
+export default Layout
